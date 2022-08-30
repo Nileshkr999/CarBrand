f1d8accfa25538fe7525ee46cf91809a29cf5e76
NILESH KUMAR SINGH <111196126+Nileshkr999@users.noreply.github.com>
2022-08-30 11:17:25 +0530

    Update Mghector.java

--- src/test/java/Brands/Mghector.java
+++ src/test/java/Brands/Mghector.java
@@ -5,6 +5,7 @@ public class Mghector {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("car name is Mghector");
+		System.out.println("car name is Mghector1");
 	}
 
 }
